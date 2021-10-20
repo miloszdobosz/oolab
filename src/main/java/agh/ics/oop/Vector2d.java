374dca56a6fee055d1e20deba4b40cd5883faed7
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2021-10-20 23:42:13 +0200

    źle zrozumiałem polecenie

--- src/main/java/agh/ics/oop/Vector2d.java
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -56,12 +56,4 @@ class Vector2d {
     public Vector2d subtract(Vector2d other) {
         return new Vector2d(this.x - other.x, this.y - other.y);
     }
-
-    public static void main(String[] args) {
-        Vector2d position1 = new Vector2d(1, 2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2, 1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
-    }
 }
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -6,6 +6,13 @@ public class World {
     public static void main(String[] args) {
         out.println("system wystartowal");
         run(transform(args));
+
+        Vector2d position1 = new Vector2d(1, 2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2, 1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
         out.println("system zakonczyl dzialanie");
     }
 
