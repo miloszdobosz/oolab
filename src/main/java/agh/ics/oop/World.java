224f807b6e1f82fd2ea32d4c47280fdbfccf7736
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2021-10-21 12:22:41 +0200

    lab1 poprawka

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -4,16 +4,15 @@ import static java.lang.System.out;
 
 public class World {
     public static void main(String[] args) {
-        out.println("system wystartowal");
+        out.println("Start");
         run(transform(args));
+        out.println("Stop");
 
         Vector2d position1 = new Vector2d(1, 2);
         System.out.println(position1);
         Vector2d position2 = new Vector2d(-2, 1);
         System.out.println(position2);
         System.out.println(position1.add(position2));
-
-        out.println("system zakonczyl dzialanie");
     }
 
     static void run(Direction[] args) {
