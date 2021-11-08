fb4d8c1e0d7d7e988006d0268585640f0b3d9e53
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2021-11-08 14:17:31 +0100

    lab4 pkt4

--- src/main/java/agh/ics/oop/Animal.java
+++ src/main/java/agh/ics/oop/Animal.java
@@ -3,14 +3,34 @@ package agh.ics.oop;
 public class Animal {
     private MapDirection orientation = MapDirection.NORTH;
     private Vector2d position = new Vector2d(2, 2);
+    private IWorldMap map;
+
+    public Animal() {
+    }
+
+    public Animal(IWorldMap map) {
+        this.map = map;
+    }
+
+    public Animal(IWorldMap map, Vector2d initialPosition) {
+        this.map = map;
+        if (!map.isOccupied(initialPosition)) {
+            map.place(this);
+        }
+    }
 
     public String toString() {
-        return orientation.toString() + ", " + position.toString();
+        return orientation.toString();
     }
 
     public void move(MoveDirection direction) {
+        this.position.add(this.orientation.toUnitVector());
 
-        // Fuuj
+        if (!this.map.canMoveTo(position)) {
+            this.position.subtract(this.orientation.toUnitVector());
+        }
+
+        /*// Fuuj
         Vector2d newPosition;
 
         switch (direction) {
@@ -30,6 +50,10 @@ public class Animal {
         case RIGHT:
             orientation = orientation.next();
             break;
-        }
+        } */
+    }
+
+    public Vector2d getPosition() {
+        return position;
     }
 }
---
+++ src/main/java/agh/ics/oop/RectangularMap.java
@@ -0,0 +1,44 @@
+package agh.ics.oop;
+
+import java.util.HashMap;
+
+public class RectangularMap implements IWorldMap{
+    private Vector2d start;
+    private Vector2d size;
+    private HashMap<Vector2d, Object> map;
+
+    public RectangularMap(int width, int height) {
+        start = new Vector2d(0, 0);
+        size = new Vector2d(width, height);
+        map = new HashMap<>(width * height);
+    }
+
+    @Override
+    public boolean canMoveTo(Vector2d position) {
+        return !isOccupied(position) && start.precedes(position) && size.follows(position);
+    }
+
+    @Override
+    public boolean place(Animal animal) {
+        if (canMoveTo(animal.getPosition())) {
+            map.put(animal.getPosition(), animal);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isOccupied(Vector2d position) {
+        return map.containsKey(position);
+    }
+
+    @Override
+    public Object objectAt(Vector2d position) {
+        return map.get(position);
+    }
+
+    public String toString() {
+        MapVisualizer visualizer = new MapVisualizer(this);
+        visualizer.draw(this.start, this.size);
+    }
+}
