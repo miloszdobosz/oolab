933e36cee6c7572d2fb5440267f9dce3d357918e
Milosz Dobosz <mdobosz@student.agh.edu.pl>
2021-11-21 20:55:48 +0100

    lab5 calosc

---
+++ src/test/java/agh/ics/oop/GrassFieldTest.java
@@ -0,0 +1,29 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GrassFieldTest {
+    @Test
+    public void test() {
+        GrassField grassField = new GrassField(100);
+
+        assertTrue(grassField.canMoveTo(new Vector2d(5, 17)));
+        assertTrue(grassField.canMoveTo(new Vector2d(-10010, 0)));
+        assertTrue(grassField.canMoveTo(new Vector2d(500, 500)));
+        assertTrue(grassField.canMoveTo(new Vector2d(-4510, -9010)));
+
+
+        Vector2d position = new Vector2d(5, 5);
+        Animal animal = new Animal(grassField, position);
+
+        grassField.place(animal);
+
+        assertTrue(grassField.isOccupied(position));
+        assertEquals(grassField.objectAt(position), animal);
+    }
+}
+
---
+++ src/test/java/agh/ics/oop/RectangularMapTest.java
@@ -0,0 +1,29 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RectangularMapTest {
+    @Test
+    public void test() {
+        RectangularMap rectangularMap = new RectangularMap(10, 10);
+
+        assertTrue(rectangularMap.canMoveTo(new Vector2d(5, 5)));
+        assertTrue(rectangularMap.canMoveTo(new Vector2d(10, 10)));
+
+        assertFalse(rectangularMap.canMoveTo(new Vector2d(-1, -1)));
+        assertFalse(rectangularMap.canMoveTo(new Vector2d(11, 11)));
+
+
+        Vector2d position = new Vector2d(5, 5);
+        Animal animal = new Animal(rectangularMap, position);
+
+        rectangularMap.place(animal);
+
+        assertTrue(rectangularMap.isOccupied(position));
+        assertEquals(rectangularMap.objectAt(position), animal);
+    }
+}
